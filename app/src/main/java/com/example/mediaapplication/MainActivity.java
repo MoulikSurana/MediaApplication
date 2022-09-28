70903eededdd8441a81fab189e904526bce49fae
MoulikSurana <moulik27surana02@gmail.com>
2022-09-28 16:11:57 +0530

    first commit

---
+++ app/src/main/java/com/example/mediaapplication/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.mediaapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private Button play,pause;
+    private SeekBar seekBar;
+    MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        play=findViewById(R.id.play);
+        pause=findViewById(R.id.pause);
+        seekBar=findViewById(R.id.seekBar);
+        mediaPlayer=MediaPlayer.create(this,R.raw.darkside);
+        mediaPlayer.start();
+        seekBar.setMax(mediaPlayer.getDuration());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+              if(b){  mediaPlayer.seekTo(i);}
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+//        mediaPlayer=new MediaPlayer();
+//        try {
+//            mediaPlayer.setDataSource("https://drive.google.com/drive/u/0/my-drive");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+//            @Override
+//            public void onPrepared(MediaPlayer mediaPlayer) {
+//                Toast.makeText(MainActivity.this, "Ready to play", Toast.LENGTH_SHORT).show();
+//                mediaPlayer.start();
+//
+//            }
+//        });
+//        mediaPlayer.prepareAsync();
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
+
+    }
+}
